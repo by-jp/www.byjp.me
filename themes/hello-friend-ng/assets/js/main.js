5633505acd57ae3b00457838094d51aa478b3d65
JP Hastings-Spital <jphastings@gmail.com>
2023-08-02 17:54:50 +0100

    Stop h-card icons on homepage, fix LCH

--- themes/hello-friend-ng/assets/js/main.js
+++ themes/hello-friend-ng/assets/js/main.js
@@ -54,7 +54,7 @@ function updateAccentColor() {
   const now = new Date()
   let start = new Date(now)
   const hue = (now - start.setHours(0,0,0,0)) / 240000
-  document.documentElement.style.setProperty('--accent', `lch(60% 180 ${hue})`);
+  document.documentElement.style.setProperty('--accent', `lch(60% 120 ${hue})`);
 }
 setInterval(updateAccentColor, 15000)
 updateAccentColor()
